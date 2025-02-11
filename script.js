3f3f8f5a18b247269eb7d9df888e5ba38b20c1f7
nam2013465456 <lyhoangnam1782013@gmail.com>
2025-02-11 14:12:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function purchaseProduct(productId) {
+    alert("Product " + productId + " has been added to your cart!");
+    // Thêm mã xử lý thanh toán ở đây
+    // Ví dụ, bạn có thể liên kết đến một trang thanh toán hoặc sử dụng PayPal
+}
